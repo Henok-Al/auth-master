fd44419998d22c313485f5d9195ad8d43da5bc36
Henok-Al <henok.gebresenbet@gmail.com>
2024-05-30 05:17:20 +0300

    error ui

--- src/app/(auth)/auth/error/page.tsx
+++ src/app/(auth)/auth/error/page.tsx
@@ -1,10 +1,11 @@
+import ErrorCard from '@/components/auth/ErrorCard'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <ErrorCard />
+    </main>
   )
 }
 
